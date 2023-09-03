f5d68fab094ffb94c2d0ced92181b51ba633cffb
ZymJames <2637117192@qq.com>
2023-09-03 18:23:17 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    // http://localhost:8080/hello
+    // http://www.baidu.com/s  协议  域名   路径
+    @RequestMapping(value="/hello", method= RequestMethod.GET)
+    public String hello(String nickname){
+        return "hello java spring"+nickname;
+
+    }
+
+    @RequestMapping(value="/hello2", method= RequestMethod.GET)
+    public String hello2(@RequestParam(value="name", required=false) String nickname){
+        return "hello java spring"+nickname;
+
+    }
+
+    //@Requestbody   user
+}
