733b16c8ca44d8b7c5d5fc865279f2955edfc551
ZymJames <2637117192@qq.com>
2023-10-01 21:25:11 +0800

    responseobject

--- src/main/java/com/example/admin/controller/ExhibitionVisitController.java
+++ src/main/java/com/example/admin/controller/ExhibitionVisitController.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.List;
-
+import com.example.admin.entity.Response.ResponseObject;
 
 @RestController
 @RequestMapping("/exhibitionVisit")
@@ -20,42 +20,46 @@ public class ExhibitionVisitController {
 
     // 应用端插入新的展区参观数据
     @RequestMapping(value="/createDataExhb", method= RequestMethod.POST)
-    public void createDataExhb(@RequestBody PostExhbVisit postExhbVisit) {
+    public ResponseObject createDataExhb(@RequestBody PostExhbVisit postExhbVisit) {
         exhibitionVisitService.createDataExhb(postExhbVisit);
+        return ResponseObject.success("查询新的展区参观数据成功");
     }
 
     // 应用端插入新的产品参观数据, 可能同时包含很多个产品
     @RequestMapping(value="/createDataProd", method= RequestMethod.POST)
-    public void createDataProd(@RequestBody PostProdVisit postProdVisit) {
+    public ResponseObject createDataProd(@RequestBody PostProdVisit postProdVisit) {
         exhibitionVisitService.createDataProd(postProdVisit);
+        return ResponseObject.success("查询新的展品参观数据成功");
     }
 
-    // 根据时间区间获得这段时间内的展区总体参观数据
+    // 管理员后台  根据时间区间获得这段时间内的展区总体参观数据
     @RequestMapping(value="/getDatabyDate", method= RequestMethod.GET)
-    public List<Exhibition_data> getDatabyDate(String startDate, String endDate){
+    public ResponseObject getDatabyDate(String startDate, String endDate){
         // startDate "2023-09-07"  endDate "2023-09-20"
         try{
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
             long startday = sdf.parse(startDate).getTime()/1000;
             long endday = sdf.parse(endDate).getTime()/1000;
             endday = endday+86400; // 终止日要加24小时,包含进当天
-            return exhibitionVisitService.getDatabyDate(startday, endday);
+            return ResponseObject.success(exhibitionVisitService.getDatabyDate(startday, endday));
         }catch (ParseException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    // 把展区的参观时长数据汇集到Excel中,发送给前端供下载
+    // 管理员后台  把展区的参观时长数据汇集到Excel中,发送给前端供下载
     @RequestMapping(value="/downloadvisitTimeDataExcel", method= RequestMethod.GET)
-    public void downloadvisitTimeDataExcel(HttpServletResponse response) throws IOException {
+    public ResponseObject downloadvisitTimeDataExcel(HttpServletResponse response) throws IOException {
         exhibitionVisitService.downloadvisitTimeDataExcel(response);
+        return ResponseObject.success("下载展区参观时长数据成功");
     }
 
     // 把展区的参观人次数据汇集到Excel中,发送给前端供下载
     @RequestMapping(value="/downloadvisitNumDataExcel", method= RequestMethod.GET)
-    public void downloadvisitNumDataExcel(HttpServletResponse response) throws IOException {
+    public ResponseObject downloadvisitNumDataExcel(HttpServletResponse response) throws IOException {
         exhibitionVisitService.downloadvisitNumDataExcel(response);
+        return ResponseObject.success("下载展区参观人次数据成功");
     }
 
 
--- src/main/java/com/example/admin/controller/InvitationCodeController.java
+++ src/main/java/com/example/admin/controller/InvitationCodeController.java
@@ -1,5 +1,5 @@
 package com.example.admin.controller;
-
+import com.example.admin.entity.Response.ResponseObject;
 import com.example.admin.entity.User.scheduled_invitation;
 import com.example.admin.entity.User.InvitationCode;
 import com.example.admin.service.InvitationCodeService;
@@ -15,29 +15,35 @@ public class InvitationCodeController {
     @Autowired
     private InvitationCodeService invitationCodeService;
 
+    // 管理员后台 生成一个不重复的激活码
     @RequestMapping(value="/generateCodeID", method= RequestMethod.GET)
-    public String generateCodeID() {
-        return invitationCodeService.generateCodeID();
+    public ResponseObject generateCodeID() {
+        return ResponseObject.success(invitationCodeService.generateCodeID());
     }
 
+    // 管理员后台  行事历获得月格式下每一天的预约数量
     @RequestMapping(value="/getScheduledDay", method= RequestMethod.GET)
-    public List<scheduled_invitation> getScheduledDay() {
-        return invitationCodeService.getScheduledDay();
+    public ResponseObject getScheduledDay() {
+        return ResponseObject.success(invitationCodeService.getScheduledDay());
     }
 
+    // 管理员后台  行事历获得周格式下每一小时的预约数量
     @RequestMapping(value="/getScheduledHour", method= RequestMethod.GET)
-    public List<scheduled_invitation> getScheduledHour() {
-        return invitationCodeService.getScheduledHour();
+    public ResponseObject getScheduledHour() {
+        return ResponseObject.success(invitationCodeService.getScheduledHour());
     }
 
+    // 管理员后台  生成一次邀请记录
     @RequestMapping(value="/createInvitation", method= RequestMethod.POST)
-    public void createInvitation(@RequestBody InvitationCode invitationCode) {
+    public ResponseObject createInvitation(@RequestBody InvitationCode invitationCode) {
         invitationCodeService.createInvitation(invitationCode);
+        return ResponseObject.success("生成邀请记录成功");
     }
 
+    // 管理员后台  获得所有的邀请记录
     @RequestMapping(value="/getAllInvitation", method= RequestMethod.GET)
-    public List<InvitationCode> getAllInvitation() {
-        return invitationCodeService.getAllInvitation();
+    public ResponseObject getAllInvitation() {
+        return ResponseObject.success(invitationCodeService.getAllInvitation());
     }
 
 
